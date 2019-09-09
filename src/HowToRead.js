00fa5b51d23c1a7386a05ce5370c73242c4c6efd
Rik Melis <rikmelis@Riks-MacBook-Air.local>
2019-09-09 15:33:43 +0200

    Add how to read as a popup

--- src/HowToRead.js
+++ src/HowToRead.js
@@ -10,46 +10,24 @@ export default class HowToRead extends React.Component {
 		this.state = {
 			open: false,
 		};
-
-    this.handleClickOutside = this.handleClickOutside.bind(this);
   }
-
-  handleClickOutside(event) {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-    	this.setState({open: false});
-    }
-  }
-
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
-	}
 
 	render() {
-		const {
-			open,
-		} = this.state;
-
 		return (
       <div className={'how-to-read'}>
-        <Question className={'question-icon'} onClick={(event) => {
-        	event.stopPropagation();
-        	event.preventDefault();
-        	this.setState({open: !open})
-        }}/>
-        {open &&
-        	<div className={'pop-up'} ref={r => this.wrapperRef = r}>
-	          <div className={'title'}>
-	          	{translate(['how_to_read', 'title'])}
-	          	{<Close className={'close-icon'} onClick={() => this.setState({open: false})}/>}
-	          </div>
-	          <div className={'paragraph'}>{translate(['how_to_read', 'p1'])}</div>
-	          <div className={'paragraph'}>{translate(['how_to_read', 'p2'])}</div>
-	          <div className={'paragraph'}>{translate(['how_to_read', 'p3'])}</div>
-	          <div className={'paragraph'}>{translate(['how_to_read', 'p4'])}</div>
+        <Question className={'question-icon'} onClick={(event) => this.setState({open: true})}/>
+        {this.state.open &&
+        	<div className={'pop-up-container'} onClick={(event) => this.setState({open: false})}>
+	        	<div className={'pop-up'} onClick={(event) => event.stopPropagation()}>
+		          <div className={'title'}>
+		          	{translate(['how_to_read', 'title'])}
+		          	{<Close className={'close-icon'} onClick={() => this.setState({open: false})}/>}
+		          </div>
+		          <div className={'paragraph'}>{translate(['how_to_read', 'p1'])}</div>
+		          <div className={'paragraph'}>{translate(['how_to_read', 'p2'])}</div>
+		          <div className={'paragraph'}>{translate(['how_to_read', 'p3'])}</div>
+		          <div className={'paragraph'}>{translate(['how_to_read', 'p4'])}</div>
+		        </div>
 	        </div>
 	      }
       </div>
